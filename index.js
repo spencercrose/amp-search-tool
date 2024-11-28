a974aa7f6436f191f0c0725fbd3c14c92bae4f63
Spencer Rose <spencer.rose@gov.bc.ca>
2024-11-28 12:02:49 -0800

    Update cors origin.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const port = 4000;
 
 // Allow CORS requests from the specified CLIENT_HOST
 const corsOptions = {
-  origin: [process.env.CLIENT_HOST],
+  origin: process.env.CLIENT_HOST,
 };
 
 app.use(cors(corsOptions));
